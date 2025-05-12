d2946e09d28dc4d834406b0e5470fde5468723bd
dennis gega <dennis.gega@uconn.edu>
2025-05-12 14:56:18 -0400

    Initialized track header and implementation files

---
+++ build/CMakeFiles/race_sim.dir/src/Strategy.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/race_sim.dir/src/Strategy.cpp.o: \
+  /Users/dennisgega/code/projects/f1_cli/src/Strategy.cpp
---
+++ build/CMakeFiles/race_sim.dir/src/Track.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/race_sim.dir/src/Track.cpp.o: \
+  /Users/dennisgega/code/projects/f1_cli/src/Track.cpp \
+  /Users/dennisgega/code/projects/f1_cli/include/Track.h
